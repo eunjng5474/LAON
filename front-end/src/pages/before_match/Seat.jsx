9da781079136e498b05457c44c4970c3a531e521
kdu201 <kdu201@naver.com>
2023-07-29 14:57:38 +0900

    feat: SEAT 페이지 레이아웃

--- front-end/src/pages/before_match/Seat.jsx
+++ front-end/src/pages/before_match/Seat.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import './styles/Seat.css'
+import parkimg from './img/LP.png'
 
 export default function Seat() {
   return (
     <div className='seat-container'>
-      <h1>Seat</h1>
+      <div className='seat-header'>
+        <h1>좌석 정보 안내</h1>
+        <div className='to-live-container'>
+          <button className='to-live-button'>
+            <h3>TO LIVE</h3>
+          </button>
+        </div>
+      </div>
+      <div className='seat-img-container'>
+        <div className='seat-home-away'>
+          <h1>HOME</h1>
+          <h1>AWAY</h1>
+        </div>
+        <img className='seat-img' src={parkimg} alt='안나와요 왜죠'/>
+      </div>
+      <div className='seat-footer'>
+        <div className='seat-scroll-bar'>
+          <h1>SEAT SCROLL</h1>
+        </div>
+        <div className='seat-search-bar'>
+          <h1>SEAT SEARCH BAR</h1>
+        </div>
+      </div>
     </div>
   )
 }
