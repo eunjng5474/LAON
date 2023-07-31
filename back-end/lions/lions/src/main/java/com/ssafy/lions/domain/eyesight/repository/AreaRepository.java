926d42105b4433db8ceeb485dbc0a20968206d1c
LeeSanghun <hoonideal@naver.com>
2023-08-01 01:31:20 +0900

    eyesight no panorama

---
+++ back-end/lions/lions/src/main/java/com/ssafy/lions/domain/eyesight/repository/AreaRepository.java
@@ -0,0 +1,12 @@
+package com.ssafy.lions.domain.eyesight.repository;
+
+import com.ssafy.lions.domain.eyesight.entity.Area;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AreaRepository extends JpaRepository<Area, Long> {
+    @Query("SELECT a.areaId FROM Area a WHERE a.areaNum = :areaNum and a.blockId = :blockId")
+    Integer findAreaNumByAreaNumAndBlockId(String blockId, int areaNum);
+}
