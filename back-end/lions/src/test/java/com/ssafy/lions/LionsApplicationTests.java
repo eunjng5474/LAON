d5164907fa8da48114f6472b61a731d27ebbae74
kimta2hwan <kimta2hwan@gmail.com>
2023-08-02 14:38:30 +0900

    test

--- back-end/lions/src/test/java/com/ssafy/lions/LionsApplicationTests.java
+++ back-end/lions/src/test/java/com/ssafy/lions/LionsApplicationTests.java
@@ -3,7 +3,7 @@ package com.ssafy.lions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class LionsApplicationTests {
 
 	@Test
