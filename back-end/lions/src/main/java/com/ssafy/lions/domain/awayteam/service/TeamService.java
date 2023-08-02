274dd50a1dbbe18c2dcd830e5c17cd56b8c23c01
kimta2hwan <kimta2hwan@gmail.com>
2023-08-02 16:15:57 +0900

    테스트

--- back-end/lions/src/main/java/com/ssafy/lions/domain/awayteam/service/TeamService.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.lions.domain.awayteam.service;
-
-import com.ssafy.lions.domain.awayteam.dto.TeamResultDto;
-
-public interface TeamService {
-    TeamResultDto getTeamInfo(int teamId);
-}
--- back-end/lions/src/main/java/com/ssafy/lions/domain/navigation/service/NaviService.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.lions.domain.navigation.service;
-
-import com.ssafy.lions.domain.navigation.dto.PointResultDto;
-
-public interface NaviService {
-    PointResultDto blockToFacility(String blockId, int facilityId);
-}
