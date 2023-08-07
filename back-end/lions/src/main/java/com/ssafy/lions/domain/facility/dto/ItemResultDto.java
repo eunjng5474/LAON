448e4b6cbe7ed5c248e802fbc78631eeef835c8d
rmsah5368 <rmsah5368@naver.com>
2023-08-07 15:35:21 +0900

    [ADD] 메뉴 데이터 추가 및 테스트 완료

--- back-end/lions/src/main/java/com/ssafy/lions/domain/facility/dto/ItemResultDto.java
+++ back-end/lions/src/main/java/com/ssafy/lions/domain/facility/dto/ItemResultDto.java
@@ -8,8 +8,18 @@ import java.util.List;
 @Data
 @Schema(description = "조회한 편의시설의 전체 메뉴 정보를 담은 객체")
 public class ItemResultDto {
-    @Schema(description = "편의시설의 전체 메뉴", example = "List<ItemDto>")
-    List<ItemDto> itemDtoList;
+    @Schema(description = "조회한 편의시설의 단품 메뉴 리스트", example = "List<ItemDto>")
+    List<ItemDto> singleItemDtoList;
+
+    @Schema(description = "조회한 편의시설의 세트 메뉴 리스트", example = "List<ItemDto>")
+    List<ItemDto> setItemDtoList;
+
+    @Schema(description = "조회한 편의시설의 사이드 메뉴 리스트", example = "List<ItemDto>")
+    List<ItemDto> sideItemDtoList;
+
+    @Schema(description = "조회한 편의시설의 음료 메뉴 리스트", example = "List<ItemDto>")
+    List<ItemDto> beverageItemDtoList;
+
     @Schema(description = "편의시설 메뉴 조회 결과", example = "1 : 조회 성공, -1 : 조회 실패")
     int result;
 }
