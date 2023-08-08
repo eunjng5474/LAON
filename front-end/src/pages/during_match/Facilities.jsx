919a6c65f549697546178c41b4a8a155781e8e68
alsuddlek1 <minyoung99311@gmail.com>
2023-08-08 14:11:48 +0900

    feat:식음료/편의시설

--- front-end/src/pages/during_match/Facilities.jsx
+++ front-end/src/pages/during_match/Facilities.jsx
@@ -20,6 +20,8 @@ export default function Facilities() {
   const [stores, setStores] = useState()
   const [facilities, setFacilities] = useState()
 
+  const [focusedBody, setFocusedBody] = useState(false)
+  // const []
 
   function selectFloor(e) {
     console.log(e)
@@ -92,8 +94,12 @@ export default function Facilities() {
     
     axios.get('https://laon.info/api/lions/facility/all')
     .then((res) => {
+      console.log(res.data)
       setFacilities(res.data.facilityList.filter(isFacility))
       setStores(res.data.facilityList.filter(isStore))
+    })
+    .catch((error) => {
+      console.log(error)
     })
 
     navigator.geolocation.getCurrentPosition(getPosition)
@@ -143,6 +149,12 @@ export default function Facilities() {
 
   },[])
 
+    function focusBody() {
+      setFocusedBody(!focusedBody)
+      console.log(focusedBody)
+    }
+
+
   return (
     <div className='facilities-container font'>
       <div className='floor-select-button'>
@@ -150,29 +162,79 @@ export default function Facilities() {
         <button onClick={selectFloor}>3F</button>
         <button onClick={selectFloor}>5F</button>
       </div>
-      <div className='facilities-body'>
+      <div className='facilities-body' onClick={focusBody}>
         <div className='facilities-navigation'>
           <img className='facilities-img' src={floor} alt=''/>  
           
           <canvas className='points-canvas' ref={naviCanvasRef}></canvas>
         </div>
         
-        <div className='facilities-select'>
+        <div className={`facilities-select ${focusedBody ? "facilities-select-focus-body" : ""}`} onClick={focusBody}>
           <div className='facilities-search-bar'>
             <div className='facilities-category'>
-              <input id="departure" onChange={onChangeDeparture} value={'현위치 : '+ currentPosition+'구역'} placeholder='출발지'/>
+              <input onClick={focusBody} id="departure" onChange={onChangeDeparture} value={'현위치 : '+ currentPosition+'구역'} placeholder='출발지'/>
             </div>
             <div className='facilities-category'>
-              <input id="destination" onChange={onChangeDestination} value={destination} placeholder='목적지'/>          
+              <input onClick={focusBody} id="destination" onChange={onChangeDestination} value={destination} placeholder='목적지'/>          
             </div>
           </div>
 
-          <div className='facilities-item-container'>
+          <div className={`facilities-item-container ${focusedBody ? "item-container-hide" : ""}`}>
             <div className='category-select'>
               <button onClick={categorySelect} className={`${category === "식음매장" ? "category-show-button" : ""}`}>식음매장</button>
               <button onClick={categorySelect} className={`${category === "편의시설" ? "category-show-button" : ""}`}>편의시설</button>
             </div>
-            <div className={`store-list ${category === "식음매장" ? "category-show" : ""}`}>
+            <div className={`store-list ${category === "편의시설" ? "store-show" : ""}`}>
+
+              
+                <div className='facilities-store'>
+                  KELLY
+                </div>
+                <div className='facilities-store'>
+                  짝태시대
+                </div>
+                <div className='facilities-store'>
+                  리얼키친더홍
+                </div>
+                <div className='facilities-store'>
+                  파파존스피자
+                </div>
+                <div className='facilities-store'>
+                  CU
+                </div>
+                <div className='facilities-store'>
+                  해피치즈스마일
+                </div>
+                <div className='facilities-store'>
+                  땅땅치킨
+                </div>
+                <div className='facilities-store'>
+                  대왕유부초밥
+                </div>
+                <div className='facilities-store'>
+                  전설꼬치
+                </div>
+                <div className='facilities-store'>
+                  버터우드
+                </div>
+                <div className='facilities-store'>
+                  알통닭강정
+                </div>
+                <div className='facilities-store'>
+                  리얼피그
+                </div>
+                <div className='facilities-store'>
+                  5직떡볶이
+                </div>
+                <div className='facilities-store'>
+                  족발슈퍼
+                </div>
+                <div className='facilities-store'>
+                  한만두
+                </div>
+                <div className='facilities-store'>
+                  블루샷
+                </div>
               
               {/* {stores && stores.map((data) => {
                 console.log(data)
@@ -182,8 +244,29 @@ export default function Facilities() {
               })} */}
             </div>
 
-            <div className={`facility-list ${category === "편의시설" ? "category-show" : ""}`}>
+            <div className={`facility-list ${category === "식음매장" ? "facility-show" : ""}`}>            
 
+                <div className='facilities-store'>
+                  여자화장실
+                </div>
+                <div className='facilities-store'>
+                  남자화장실
+                </div>
+                <div className='facilities-store'>
+                  쓰레기통
+                </div>
+                <div className='facilities-store'>
+                  남자 장애인 화장실
+                </div>
+                <div className='facilities-store'>
+                  여자 장애인 화장실
+                </div>
+                <div className='facilities-store'>
+                  수유실
+                </div>
+                <div className='facilities-store'>
+                  흡연실
+            </div>
             </div>
             
           </div>
