0d9918ee4c4ccfda95209cd5f6be630fa8ad70d8
이상훈 <hoonideal@naver.com>
2023-08-09 10:46:41 +0900

    SectionDetail test

--- front-end/src/pages/before_match/SectionDetail.jsx
+++ front-end/src/pages/before_match/SectionDetail.jsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import { useLocation } from 'react-router-dom';
 import { useNavigate } from 'react-router'
-import img_3_4_1 from './img/3_4_1.jpeg'
+import img_3_4_1 from './images/3-1/1.jpg'
 import arrow from './img/arrow.png'
 import ImageMap from 'image-map';
 import mapImg from './img/sectionMap.png'
