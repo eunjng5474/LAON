617507c3d43014836171b416a095212bdf7075c1
LeeSanghun <hoonideal@naver.com>
2023-08-10 11:39:05 +0900

    SectionDetail.jsx

--- front-end/src/pages/before_match/SectionDetail.jsx
+++ front-end/src/pages/before_match/SectionDetail.jsx
@@ -1,8 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import { useLocation, useSearchParams } from 'react-router-dom';
 import { useNavigate } from 'react-router'
-import img_3_4_1 from './img/3_4_1.jpeg'
-// import img_3_4_1 from './images/3-4/1.jpg'
 
 import arrow from './img/arrow.png'
 import ImageMap from 'image-map';
@@ -15,34 +13,32 @@ import { useSelector } from 'react-redux';
 
 export default function SectionDetail() {
   const { state } = useLocation();
-  const seatName = state;
+  const seatName = state.sectionSelect;
   const [panoramaSrc, setPanoramaSrc] = useState()
   const navigate = useNavigate();
-
 
   const seatsSeat = useSelector((state) => state.seatsSeat)
   // const seatSelect = useSelector((state) => state.seatSelect)
 
   console.log(seatsSeat)
-  let seatUrl = "laon.info/images/view/";
+  let seatUrl = "https://laon.info/images/view/";
   const seatsSeat2 = seatsSeat.substr(1);
   const seatsSeatList = seatsSeat2.split('_');
   console.log(seatsSeatList)
   if (seatsSeatList.length == 2) {
-    seatUrl += seatsSeatList[0] +"/"+ seatsSeatList[1];
+    seatUrl += seatsSeatList[0] +"/"+ seatsSeatList[1] + ".jpg";
   } else if (seatsSeatList.length == 3) {
-    seatUrl += seatsSeatList[0] + "-" +seatsSeat[1] + "/" + seatsSeatList[2];
+    seatUrl += seatsSeatList[0] + "-" +seatsSeatList[1] + "/" + seatsSeatList[2] + ".jpg";
   }
+  console.log(seatUrl);
 
   // const seatImg = eval(seatSelect)
 
   function toSeat() {
     navigate('/seat');
-    // console.log(selectSeat);
   }
 
   function getCoordinate(event) {
-    // const child = event.target.children[0]
     const x = event.pageX;
     const y = event.pageY;
   }
@@ -63,18 +59,19 @@ export default function SectionDetail() {
 
         <div className='section-detail-img'>
           <img src={seatUrl}></img>
-        </div>
+          </div>
 
         <div className='map-controller'>
-          <img  src='https://laon.info/images/sectionSelect/SKY1.png' useMap="#image-map"/>
+          <img  src={`https://laon.info/images/sectionSelect/${seatName}.png`} useMap="#image-map"/>
 
           <map name="image-map">
-              <area onClick={getCoordinate} target="" alt="" title="" coords="63,217,63,232,84,232,70,217" shape="poly"/>
-              <area onClick={getCoordinate} target="" alt="" title="" coords="137,281,137,306,165,306" shape="poly"/>
-              <area onClick={getCoordinate} target="" alt="" title="" coords="137,310,166,310,166,325,137,325" shape="poly"/>
+            <area onClick={getCoordinate} target="" alt="" title="" coords="63,217,63,232,84,232,70,217" shape="poly"/>
+            <area onClick={getCoordinate} target="" alt="" title="" coords="137,281,137,306,165,306" shape="poly"/>
+            <area onClick={getCoordinate} target="" alt="" title="" coords="137,310,166,310,166,325,137,325" shape="poly"/>
           </map>
         </div>
       </div>
     </div>
   )
 }
+
