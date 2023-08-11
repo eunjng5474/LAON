b95b947c29e1ee1d6fddada3e3ec2097c865bf38
eunjng5474 <eunjng5474@naver.com>
2023-08-12 00:12:45 +0900

    feat: 내비 층 변경 setTimeout으로는 함

--- front-end/src/pages/during_match/Navigation.jsx
+++ front-end/src/pages/during_match/Navigation.jsx
@@ -12,10 +12,41 @@ export default function Navigation() {
   const [floor, setFloor] = useState(map3F)
   const [pointDtoList, setPointDtoList] = useState()
   const [nextPointDtoList, setNextPointDtoList] = useState()
+
+  const [showNextPoints, setshowNextPoints] = useState(false)
   // const [currentFloor, setCurrentFloor] = useState('3F')
 
 
+  function draw() {
+    if (!pointDtoList) {
+      return
+    }
 
+    const canvas = document.getElementById('navi-canvas')
+    canvas.width = "412"
+    canvas.height = "462"
+    // canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    const ctx = canvas.getContext('2d')
+
+
+    ctx.beginPath()
+    ctx.moveTo(pointDtoList[0].x, pointDtoList[0].y)
+    for (let i = 0; i < pointDtoList.length; i++) {
+      ctx.lineTo(pointDtoList[i].x, pointDtoList[i].y)
+      // if (pointDtoList[i].type === 'S') {
+      //   setPointDtoList(pointDtoList => {
+      //     pointDtoList = nextPointDtoList
+      //     console.log(pointDtoList)
+      //     // setFloor(map2F)
+      //     // draw()
+      //     return pointDtoList
+      //   })
+      // }
+    }
+    ctx.stroke()
+
+    requestAnimationFrame(draw)
+  }
 
 
   // const naviGoal = data.facilityName;
@@ -26,7 +57,6 @@ export default function Navigation() {
   //   console.log(e.nativeEvent.offsetX, e.nativeEvent.offsetY)
   //   const x = e.nativeEvent.offsetX
   //   const y = e.nativeEvent.offsetY
-
   // }
 
   function goAR() {
@@ -38,37 +68,6 @@ export default function Navigation() {
 
   useEffect(() => {
 
-    function draw() {
-      if (!pointDtoList) {
-        return
-      }
-  
-      const canvas = document.getElementById('navi-canvas')
-      canvas.width = "412"
-      canvas.height = "462"
-      // canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-  
-      
-      const ctx = canvas.getContext('2d')
-  
-      ctx.beginPath()
-      ctx.moveTo(pointDtoList[0].x, pointDtoList[0].y)
-      for (let i = 0; i < pointDtoList.length; i++) {
-        ctx.lineTo(pointDtoList[i].x, pointDtoList[i].y)
-        if (pointDtoList[i].type === 'S') {
-          setPointDtoList(pointDtoList => {
-            pointDtoList = nextPointDtoList
-            console.log(pointDtoList)
-            return pointDtoList
-          })
-        }
-      }
-      ctx.stroke()
-
-      // requestAnimationFrame(draw)
-    }
-    draw()
-
     axios.get('https://laon.info/api/lions/route/3-1/파파존스')
     .then((res) => {
       for (let i = 0; i < res.data.pointDtoList.length; i++) {
@@ -77,20 +76,40 @@ export default function Navigation() {
             pointDtoList = res.data.pointDtoList.slice(0, i+1)
             console.log('현재 리스트')
             console.log(pointDtoList)
+            draw(pointDtoList)
             return pointDtoList
           })
           setNextPointDtoList(nextPointDtoList => {
             nextPointDtoList = res.data.pointDtoList.slice(i+1, res.data.pointDtoList.length)
             console.log('다음 리스트')
             console.log(nextPointDtoList)
+            draw(nextPointDtoList)
             return nextPointDtoList
           })
-          draw()
+          // setshowNextPoints(true)
           break
         }
       }
     })
+
   }, [])
+
+  useEffect(() => {
+    draw()
+    setshowNextPoints(true)
+
+  }, [pointDtoList])
+
+  useEffect(() => {
+    if(showNextPoints){
+      setTimeout(() => {
+        setFloor(map2F)
+        setPointDtoList(nextPointDtoList)
+        draw()
+      }, 2000)
+    }
+  }, [nextPointDtoList])
+
 
   return (
     <div className='navigation-container font'>
