7b2407f4d3dc3f057596040c401de61b649da169
eunjng5474 <eunjng5474@naver.com>
2023-08-11 21:33:06 +0900

    feat: sectionDetail 3루 내외야 및 중앙테이블 좌표 완료

--- front-end/src/pages/before_match/SectionDetail.jsx
+++ front-end/src/pages/before_match/SectionDetail.jsx
@@ -453,6 +453,611 @@ export default function SectionDetail() {
         return seatUrl
       })
     }
+
+    // 3층 어웨이
+    else if (460 <= x && x <= 477 && 192 <= y && y <= 248) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "GRASS/3.jpg"
+        return seatUrl
+      })
+    }
+    else if (460 <= x && x <= 477 && 257 <= y && y <= 307) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "GRASS/2.jpg"
+        return seatUrl
+      })
+    }
+    else if (460 <= x && x <= 477 && 317 <= y && y <= 370) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "GRASS/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (405 <= x && x <= 420 && 218 <= y && y <= 229) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1-12/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (402 <= x && x <= 422 && 235 <= y && y <= 248) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1-11/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (389 <= x && x <= 397 && 253 <= y && y <= 267) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1-10/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (402 <= x && x <= 422 && 255 <= y && y <= 267) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1-10/2.jpg"
+        return seatUrl
+      })
+    }
+    else if (355 <= x && x <= 372 && 276 <= y && y <= 285) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1-9/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (379 <= x && x <= 396 && 273 <= y && y <= 285) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1-9/2.jpg"
+        return seatUrl
+      })
+    }
+    else if (404 <= x && x <= 421 && 273 <= y && y <= 285) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1-9/3.jpg"
+        return seatUrl
+      })
+    }
+    else if (355 <= x && x <= 372 && 292 <= y && y <= 304) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1-8/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (379 <= x && x <= 396 && 292 <= y && y <= 304) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1-8/2.jpg"
+        return seatUrl
+      })
+    }
+    else if (404 <= x && x <= 421 && 292 <= y && y <= 304) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1-8/3.jpg"
+        return seatUrl
+      })
+    }
+    else if (355 <= x && x <= 372 && 310 <= y && y <= 322) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1-7/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (379 <= x && x <= 396 && 310 <= y && y <= 322) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1-7/2.jpg"
+        return seatUrl
+      })
+    }
+    else if (404 <= x && x <= 421 && 310 <= y && y <= 322) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1-7/3.jpg"
+        return seatUrl
+      })
+    }
+    else if (356 <= x && x <= 373 && 328 <= y && y <= 334) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1-6/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (379 <= x && x <= 396 && 328 <= y && y <= 337) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1-6/2.jpg"
+        return seatUrl
+      })
+    }
+    else if (404 <= x && x <= 421 && 329 <= y && y <= 356) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1-6/3.jpg"
+        return seatUrl
+      })
+    }
+    else if (324 <= x && x <= 350 && 290 <= y && y <= 303) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1E-3/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (324 <= x && x <= 350 && 310 <= y && y <= 321) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1E-2/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (324 <= x && x <= 350 && 328 <= y && y <= 340) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1E-1/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (360 <= x && x <= 370 && 337 <= y && y <= 343) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1-5/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (376 <= x && x <= 394 && 347 <= y && y <= 355) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1-5/2.jpg"
+        return seatUrl
+      })
+    }
+    else if (393 <= x && x <= 412 && 360 <= y && y <= 372) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1-5/3.jpg"
+        return seatUrl
+      })
+    }
+    else if (398 <= x && x <= 418 && 367 <= y && y <= 377) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1-5/4.jpg"
+        return seatUrl
+      })
+    }
+    else if (343 <= x && x <= 360 && 340 <= y && y <= 355) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1-4/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (363 <= x && x <= 375 && 360 <= y && y <= 371) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1-4/2.jpg"
+        return seatUrl
+      })
+    }
+    else if (377 <= x && x <= 387 && 374 <= y && y <= 385) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1-4/3.jpg"
+        return seatUrl
+      })
+    }
+    else if (387 <= x && x <= 394 && 385 <= y && y <= 391) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1-4/4.jpg"
+        return seatUrl
+      })
+    }
+    else if (332 <= x && x <= 345 && 352 <= y && y <= 365) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1-3/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (347 <= x && x <= 362 && 370 <= y && y <= 383) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1-3/2.jpg"
+        return seatUrl
+      })
+    }
+    else if (362 <= x && x <= 375 && 387 <= y && y <= 395) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1-3/3.jpg"
+        return seatUrl
+      })
+    }
+    else if (375 <= x && x <= 382 && 396 <= y && y <= 402) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1-3/4.jpg"
+        return seatUrl
+      })
+    }
+    else if (319 <= x && x <= 333 && 362 <= y && y <= 376) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1-2/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (336 <= x && x <= 349 && 381 <= y && y <= 393) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1-2/2.jpg"
+        return seatUrl
+      })
+    }
+    else if (352 <= x && x <= 363 && 398 <= y && y <= 407) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1-2/3.jpg"
+        return seatUrl
+      })
+    }
+    else if (360 <= x && x <= 370 && 407 <= y && y <= 413) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1-2/4.jpg"
+        return seatUrl
+      })
+    }
+    else if (311 <= x && x <= 322 && 373 <= y && y <= 383) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1-1/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (327 <= x && x <= 340 && 389 <= y && y <= 400) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1-1/2.jpg"
+        return seatUrl
+      })
+    }
+    else if (342 <= x && x <= 349 && 407 <= y && y <= 415) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1-1/3.jpg"
+        return seatUrl
+      })
+    }
+    else if (353 <= x && x <= 357 && 418 <= y && y <= 421) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "1-1/4.jpg"
+        return seatUrl
+      })
+    }
+    else if (303 <= x && x <= 316 && 379 <= y && y <= 391) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "T1-4/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (317 <= x && x <= 327 && 396 <= y && y <= 408) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "T1-4/2.jpg"
+        return seatUrl
+      })
+    }
+    else if (335 <= x && x <= 342 && 414 <= y && y <= 422) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "T1-4/3.jpg"
+        return seatUrl
+      })
+    }
+    else if (345 <= x && x <= 349 && 425 <= y && y <= 430) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "T1-4/4.jpg"
+        return seatUrl
+      })
+    }
+    else if (290 <= x && x <= 304 && 387 <= y && y <= 401) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "T1-3/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (308 <= x && x <= 320 && 406 <= y && y <= 419) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "T1-3/2.jpg"
+        return seatUrl
+      })
+    }
+    else if (323 <= x && x <= 335 && 423 <= y && y <= 433) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "T1-3/3.jpg"
+        return seatUrl
+      })
+    }
+    else if (333 <= x && x <= 340 && 432 <= y && y <= 439) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "T1-3/4.jpg"
+        return seatUrl
+      })
+    }
+    else if (278 <= x && x <= 292 && 400 <= y && y <= 413) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "T1-2/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (297 <= x && x <= 311 && 417 <= y && y <= 430) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "T1-2/2.jpg"
+        return seatUrl
+      })
+    }
+    else if (310 <= x && x <= 321 && 432 <= y && y <= 443) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "T1-2/3.jpg"
+        return seatUrl
+      })
+    }
+    else if (321 <= x && x <= 329 && 443 <= y && y <= 450) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "T1-2/4.jpg"
+        return seatUrl
+      })
+    }
+    else if (268 <= x && x <= 282 && 410 <= y && y <= 425) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "T1-1/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (280 <= x && x <= 296 && 430 <= y && y <= 444) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "T1-1/2.jpg"
+        return seatUrl
+      })
+    }
+    else if (289 <= x && x <= 315 && 450 <= y && y <= 470) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "T1-1/3.jpg"
+        return seatUrl
+      })
+    }
+    // 중앙
+    else if (255 <= x && x <= 260 && 400 <= y && y <= 411) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "VIP1/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (225 <= x && x <= 230 && 400 <= y && y <= 411) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "VIP3/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (255 <= x && x <= 267 && 416 <= y && y <= 433) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "TC-1/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (235 <= x && x <= 252 && 416 <= y && y <= 433) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "TC-2/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (219 <= x && x <= 230 && 416 <= y && y <= 433) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "TC-3/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (255 <= x && x <= 276 && 439 <= y && y <= 457) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "TC-1/2.jpg"
+        return seatUrl
+      })
+    }
+    else if (235 <= x && x <= 252 && 439 <= y && y <= 457) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "TC-2/2.jpg"
+        return seatUrl
+      })
+    }
+    else if (211 <= x && x <= 230 && 439 <= y && y <= 457) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "TC-3/2.jpg"
+        return seatUrl
+      })
+    }
+    else if (255 <= x && x <= 285 && 465 <= y && y <= 483) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "TC-1/3.jpg"
+        return seatUrl
+      })
+    }
+    else if (235 <= x && x <= 252 && 465 <= y && y <= 483) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "TC-2/3.jpg"
+        return seatUrl
+      })
+    }
+    else if (201 <= x && x <= 230 && 465 <= y && y <= 483) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "TC-3/3.jpg"
+        return seatUrl
+      })
+    }
+
+    // 외야 1루
+    else if (362 <= x && x <= 385 && 77 <= y && y <= 98) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "F-2/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (416 <= x && x <= 441 && 133 <= y && y <= 157) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "F-1/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (383 <= x && x <= 399 && 220 <= y && y <= 232) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "TR-0/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (401 <= x && x <= 415 && 202 <= y && y <= 214) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "TR-0/2.jpg"
+        return seatUrl
+      })
+    }
+    else if (409 <= x && x <= 421 && 176 <= y && y <= 185) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "TR-1/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (395 <= x && x <= 405 && 162 <= y && y <= 172) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "TR-2/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (384 <= x && x <= 394 && 150 <= y && y <= 160) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "TR-3/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (372 <= x && x <= 383 && 138 <= y && y <= 148) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "TR-4/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (360 <= x && x <= 371 && 125 <= y && y <= 135) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "TR-5/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (349 <= x && x <= 359 && 113 <= y && y <= 123) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "TR-6/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (335 <= x && x <= 345 && 101 <= y && y <= 111) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "TR-7/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (285 <= x && x <= 330 && 62 <= y && y <= 88) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "TR-9/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (265 <= x && x <= 277 && 62 <= y && y <= 72) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "TR-10/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (385 <= x && x <= 399 && 193 <= y && y <= 212) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "RF-1/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (377 <= x && x <= 385 && 180 <= y && y <= 199) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "RF-2/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (360 <= x && x <= 372 && 170 <= y && y <= 187) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "RF-3/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (350 <= x && x <= 360 && 158 <= y && y <= 175) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "RF-4/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (340 <= x && x <= 350 && 146 <= y && y <= 163) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "RF-5/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (328 <= x && x <= 338 && 134 <= y && y <= 152) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "RF-6/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (316 <= x && x <= 326 && 121 <= y && y <= 138) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "RF-7/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (304 <= x && x <= 314 && 108 <= y && y <= 126) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "RF-8/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (283 <= x && x <= 303 && 94 <= y && y <= 115) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "RF-9/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (264 <= x && x <= 278 && 92 <= y && y <= 115) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "RF-10/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (267 <= x && x <= 377 && 214 <= y && y <= 224) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "MR-1/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (356 <= x && x <= 365 && 202 <= y && y <= 212) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "MR-2/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (345 <= x && x <= 353 && 190 <= y && y <= 200) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "MR-3/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (333 <= x && x <= 342 && 179 <= y && y <= 189) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "MR-4/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (320 <= x && x <= 328 && 166 <= y && y <= 176) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "MR-5/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (307 <= x && x <= 318 && 153 <= y && y <= 165) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "MR-6/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (297 <= x && x <= 304 && 142 <= y && y <= 152) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "MR-7/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (283 <= x && x <= 293 && 126 <= y && y <= 143) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "MR-8/1.jpg"
+        return seatUrl
+      })
+    }
+    else if (265 <= x && x <= 280 && 118 <= y && y <= 130) {
+      setSeatUrl(seatUrl => {
+        seatUrl = imageUrl + "MR-10/1.jpg"
+        return seatUrl
+      })
+    }
   }
 
   useEffect(() => {
