b927008863c4b9312f389cd2272e8d8c69bbe760
eunjng5474 <eunjng5474@naver.com>
2023-08-11 11:24:04 +0900

    feat: 내비 좌표 찍기 완

--- front-end/src/pages/during_match/Navigation.jsx
+++ front-end/src/pages/during_match/Navigation.jsx
@@ -2,13 +2,13 @@ import React, { useEffect, useState, useRef } from 'react'
 import './styles/Navigation.css';
 import navigationMap from './img/navigation.png'
 // import EntireSectionMapImg from './img/sectionMap.png';
-import map2F from './img/sectionMap_2F.png';
-import map3F from './img/sectionMap_3F.png';
-import map5F from './img/sectionMap_5F.png';
+import map2F from './img/coordinate_2F.png';
+import map3F from './img/coordinate_3F.png';
+import map5F from './img/coordinate_5F.png';
 
 export default function Navigation() {
   const naviCanvasRef = useRef(null);
-  const [floor, setFloor] = useState(navigationMap)
+  const [floor, setFloor] = useState(map2F)
   // const [currentFloor, setCurrentFloor] = useState('3F')
 
   // const naviGoal = data.facilityName;
@@ -23,27 +23,19 @@ export default function Navigation() {
     6: {pointId: 307, pointName: '3-7', type: 'G', x: 110, y: 390},
     7: {pointId: 306, pointName: '3-8', type: 'G', x: 140, y: 420},
     8: {pointId: 305, pointName: '3-9', type: 'G', x: 200, y: 420},
-    9: {pointId: 304, pointName: '3-10 기둥 근처 계단', type: 'S', x: 200, y: 460},
-    10: {pointId: 227, pointName: 'Food Street 내부 계단', type: 'S', x: 5, y: 160},
-    11: {pointId: 225, pointName: '2층 게이트3', type: 'G', x: 20, y: 200},
-    12: {pointId: 224, pointName: '2층 게이트2', type: 'G', x: 20, y: 260}
+    9: {pointId: 304, pointName: '3-10 기둥 근처 계단', type: 'S', x: 200, y: 450},
+    10: {pointId: 227, pointName: 'Food Street 내부 계단', type: 'S', x: 200, y: 450},
+    11: {pointId: 225, pointName: '2층 게이트3', type: 'G', x: 160, y: 450},
+    12: {pointId: 224, pointName: '2층 게이트2', type: 'G', x: 70, y: 360}
   }
 
-  // function selectFloor(e) {
-  //   console.log(e)
-  //   if (e.target.innerText === '2F') {
-  //     setFloor(map2F)
-  //     setCurrentFloor('2F')
-  //   }
-  //   else if (e.target.innerText === '3F') {
-  //     setFloor(map3F)
-  //     setCurrentFloor('3F')
-  //   }
-  //   else if (e.target.innerText === '5F') {
-  //     setFloor(map5F)
-  //     setCurrentFloor('5F')
-  //   }
+  // function getCoordinate(e) {
+  //   console.log(e.nativeEvent.offsetX, e.nativeEvent.offsetY)
+  //   const x = e.nativeEvent.offsetX
+  //   const y = e.nativeEvent.offsetY
+
   // }
+
 
   // function selectStore(e) {
   //   setDestination(e.target.id)
