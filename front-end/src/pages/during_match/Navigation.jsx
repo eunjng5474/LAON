cd99b51ac9eac2f08431375903906cae1d81064d
eunjng5474 <eunjng5474@naver.com>
2023-08-12 17:57:39 +0900

    feat: 내비 속도 조절 됨

--- front-end/src/pages/during_match/Navigation.jsx
+++ front-end/src/pages/during_match/Navigation.jsx
@@ -17,7 +17,10 @@ export default function Navigation() {
   // const [currentFloor, setCurrentFloor] = useState('3F')
 
   let i = 1;
-  function startDraw() {
+  
+  function startDraw(list) {
+    let waypoints = [];
+
     if (!pointDtoList) {
       return
     }
@@ -25,20 +28,63 @@ export default function Navigation() {
     canvas.width = "412"
     canvas.height = "462"
     const ctx = canvas.getContext('2d')
-    ctx.beginPath()
-    ctx.moveTo(pointDtoList[0].x, pointDtoList[0].y)
 
-    function draw() {
-      if (i < pointDtoList.length) {
-        ctx.lineTo(pointDtoList[i].x, pointDtoList[i].y)
-        ctx.stroke()
-        i ++;
-        requestAnimationFrame(draw)
+    // let i=1;
+    for(i; i < list.length; i++){
+    // for(let i=1; i<pointDtoList.length; i++){
+      let pt0 = list[i-1];
+      let pt1 = list[i];
+      let dx = pt1.x - pt0.x;
+      let dy = pt1.y - pt0.y;
+
+      // 10 숫자 늘리면 속도 느려짐
+      for(let j=0; j<10; j++){
+        let x=pt0.x+dx*j/10;
+        let y=pt0.y+dy*j/10;
+        waypoints.push({x:x, y:y});
       }
     }
+    
+    let t=1;
+    draw();
 
-    draw()
+    console.log(waypoints)
+
+    // ctx.beginPath()
+    // ctx.moveTo(waypoints[0].x, waypoints[0].y)
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    function draw() {
+      if (t < waypoints.length-1) {
+        requestAnimationFrame(draw);
+      }
+      else if (t === waypoints.length - 1) {
+        setshowNextPoints(true)
+      }
+      ctx.beginPath()
+      ctx.moveTo(waypoints[t-1].x, waypoints[t-1].y)
+        ctx.lineTo(waypoints[t].x, waypoints[t].y)
+        ctx.lineWidth = '10';
+        ctx.lineCap = 'round';
+        ctx.strokeStyle = '#074CA1';
+        ctx.stroke()
+        t++;
+
+        // 이러면 속도는 느려지는데 끊겨서 그려지는 듯한 느낌
+        // if( i < pointDtoList.length){
+        //     setTimeout(() => {
+        //       requestAnimationFrame(draw)
+        //     }, 50)
+        //   }
+        // }
+    }
+
+    // draw()
+    // if(i === pointDtoList.length){
+    //   setshowNextPoints(true)
+    // }
   }
+
 
 
 
@@ -67,16 +113,16 @@ export default function Navigation() {
         if (res.data.pointDtoList[i].type === 'S') {
           setPointDtoList(pointDtoList => {
             pointDtoList = res.data.pointDtoList.slice(0, i+1)
-            console.log('현재 리스트')
-            console.log(pointDtoList)
-            startDraw(pointDtoList)
+            // console.log('현재 리스트')
+            // console.log(pointDtoList)
+            // startDraw(pointDtoList)
             return pointDtoList
           })
           setNextPointDtoList(nextPointDtoList => {
             nextPointDtoList = res.data.pointDtoList.slice(i+1, res.data.pointDtoList.length)
-            console.log('다음 리스트')
-            console.log(nextPointDtoList)
-            startDraw(nextPointDtoList)
+            // console.log('다음 리스트')
+            // console.log(nextPointDtoList)
+            // startDraw(nextPointDtoList)
             return nextPointDtoList
           })
           // setshowNextPoints(true)
@@ -88,10 +134,8 @@ export default function Navigation() {
   }, [])
 
   useEffect(() => {
-    startDraw()
-    setshowNextPoints(true)
-
-
+    startDraw(pointDtoList)
+    // setshowNextPoints(true)
   }, [pointDtoList])
 
   useEffect(() => {
@@ -99,10 +143,10 @@ export default function Navigation() {
       setTimeout(() => {
         setFloor(map2F)
         setPointDtoList(nextPointDtoList)
-        startDraw()
+        startDraw(nextPointDtoList)
       }, 500)
     }
-  }, [nextPointDtoList])
+  }, [nextPointDtoList, showNextPoints])
 
 
   return (
